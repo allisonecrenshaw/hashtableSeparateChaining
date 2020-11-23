a4abb445e470e108d7bad6dc2cd665d1e1506189
Allison Elizabeth <allisonelizabeth@Allisons-Air.lan1>
2020-11-22 22:22:47 -0600

    All functions stubbed out.

--- hashtable.cpp
+++ hashtable.cpp
@@ -17,3 +17,44 @@ HashTable::HashTable() {
 HashTable::~HashTable() {
 
 }
+
+/*******************************
+ * insert, get, remove
+*******************************/
+bool HashTable::insertEntry(int, string) {
+    bool ok = false;
+    return ok;
+}
+
+string HashTable::getData(int) {
+    string data = "";
+    return data;
+}
+
+bool HashTable::removeEntry(int) {
+    bool ok = false;
+    return ok;
+}
+
+
+/*******************************
+ * getCount and printTable
+*******************************/
+int HashTable::getCount() {
+    return count;
+}
+
+void HashTable::printTable() {
+    string line = "******************************\n";
+    cout << line << "Printing Hash Table: " << endl;
+    cout << "End of Hash Table." << endl << line;
+}
+
+
+/*******************************
+ * private helper functions
+*******************************/
+// private helper method for hashing
+int HashTable::hash(int id) {
+    return id % HASHTABLESIZE;
+}
