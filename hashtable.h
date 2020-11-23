90272d51bdeaa083c1f951f769175bfcf8193ec2
Allison Elizabeth <allisonelizabeth@Allisons-Air.lan1>
2020-11-22 21:56:21 -0600

    Added defines, includes for io and linked list, plus function prototypes.

--- hashtable.h
+++ hashtable.h
@@ -4,3 +4,35 @@ Coding 06
 Purpose: Hashmap program using
  embedded linked list.
 **********************/
+
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
+#define HASHTABLESIZE 15
+
+#include "linkedlist.h"
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+class HashTable {
+
+public:
+    HashTable(); // constructor
+    ~HashTable(); // destructor
+
+    bool insertEntry(int, string);
+    string getData(int);
+    bool removeEntry(int);
+    int getCount();
+    void printTable();
+
+private:
+    int hash(int);
+    int count;
+    Data *hashtable[HASHTABLESIZE];
+};
+
+
+#endif
