1c80fb75fcc7ce3f1051d7c221f2d6a22f2a658b
Allison Elizabeth <allisonelizabeth@Allisons-Air.lan1>
2020-11-27 13:21:03 -0600

    printTable written. Working when empty, need to test again to see if working when full, will do those tests when insert method is complete.

--- hashtable.cpp
+++ hashtable.cpp
@@ -12,18 +12,37 @@ Purpose: Hashmap program using
 *******************************/
 HashTable::HashTable() {
     count = 0;
-    LinkedList hashtable[HASHTABLESIZE];
+    for (int i=0; i < HASHTABLESIZE; i++) {
+        hashtable[i] = new LinkedList;
+    }
 }
 
 HashTable::~HashTable() {
-
+    for (int i=0; i < HASHTABLESIZE; i++) {
+        hashtable[i]->clearList();
+    }
 }
 
 /*******************************
  * insert, get, remove
 *******************************/
-bool HashTable::insertEntry(int, string) {
+bool HashTable::insertEntry(int id, string info) {
     bool ok = false;
+    bool dupe = false;
+    bool positive = false;
+    int loop = 0;
+
+    // check for positive and non-duplicate ids
+    cout << "Starting insert entry." << endl;
+    if (id > 0)
+        positive = true;
+    while (dupe == false && loop < HASHTABLESIZE) {
+        dupe = hashtable[loop]->exists(id);
+    }
+    if (positive == true && dupe == false) {
+        cout << "Data is ok. Will fill in insert code here later.";
+    }
+
     return ok;
 }
 
@@ -48,6 +67,15 @@ int HashTable::getCount() {
 void HashTable::printTable() {
     string line = "******************************\n";
     cout << line << "Printing Hash Table: " << endl;
+    for (int i = 0; i < HASHTABLESIZE; i++) {
+        cout << i+1 << ": ";
+        if (hashtable[i]->getCount() == 0) {
+            cout << "Empty" << endl;
+        }
+        else {
+            cout << hashtable[i] << endl;
+        }
+    }
     cout << "End of Hash Table." << endl << line;
 }
 
